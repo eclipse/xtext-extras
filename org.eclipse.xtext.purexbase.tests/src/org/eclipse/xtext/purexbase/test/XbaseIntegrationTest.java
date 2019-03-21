137d2b883f0aa695e07d4f174e16ae464cb8855f
Eva Poell <epoell@itemis.com>
2019-03-21 16:35:28 +0100

    [eclipse/xtext-xtend#395] Tests for (Pure)XBase Interpeter and Compiler
    Signed-off-by: Eva Poell <epoell@itemis.com>

--- org.eclipse.xtext.purexbase.tests/src/org/eclipse/xtext/purexbase/test/XbaseIntegrationTest.java
+++ org.eclipse.xtext.purexbase.tests/src/org/eclipse/xtext/purexbase/test/XbaseIntegrationTest.java
@@ -22,12 +22,12 @@ import org.junit.runner.RunWith;
 
 import com.google.inject.Inject;
 
-
 @RunWith(XtextRunner.class)
 @InjectWith(RuntimeInjectorProvider.class)
 public class XbaseIntegrationTest extends AbstractXbaseEvaluationTest {
-	
-	@Inject private CompilationTestHelper helper;
+
+	@Inject
+	private CompilationTestHelper helper;
 
 	@Override
 	protected Object invokeXbaseExpression(String expression) throws Exception {
@@ -53,45 +53,171 @@ public class XbaseIntegrationTest extends AbstractXbaseEvaluationTest {
 			throw exception[0];
 		return result[0];
 	}
-	
+
 	@Override
 	@Test
 	@Ignore
 	public void testImplicitOneArgClosure_01() throws Exception {
 		super.testImplicitOneArgClosure_01();
 	}
-	
+
 	@Override
-	@Test 
+	@Test
 	@Ignore("Wrong type")
 	public void testReturnExpression_07() throws Exception {
 		assertEvaluatesTo(null, "return if (true) while(false) ('foo'+'bar').length else null");
 		assertEvaluatesTo(null, "return if (false) while(false) ('foo'+'bar').length else null");
 	}
-	
+
 	@Override
-	@Test 
+	@Test
 	@Ignore("Wrong type")
 	public void testReturnExpression_08() throws Exception {
 		assertEvaluatesTo(null, "return if (true) while(false) ('foo'+'bar').length else 'zonk'");
 		assertEvaluatesTo("zonk", "return if (false) while(false) ('foo'+'bar').length else 'zonk'");
 	}
-	
+
 	/**
 	 * @since 2.5
 	 */
 	@Override
-	@Test public void testIfExpression_06() throws Exception {
-		assertEvaluatesTo(0, 
-				"if (Boolean.FALSE) return 1");
+	@Test
+	public void testIfExpression_06() throws Exception {
+		assertEvaluatesTo(0, "if (Boolean.FALSE) return 1");
 	}
-	
+
 	/**
 	 * @since 2.5
 	 */
 	@Override
-	@Test public void testIfExpression_07() throws Exception {
-		assertEvaluatesTo(0l, 
-				"if (Boolean.FALSE) return 1L");
+	@Test
+	public void testIfExpression_07() throws Exception {
+		assertEvaluatesTo(0l, "if (Boolean.FALSE) return 1L");
+	}
+
+	@Test
+	@Override
+	public void testTryWithoutResources() throws Exception {
+		super.testTryWithoutResources();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_easy() throws Exception {
+		super.testTryWithResources_easy();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_lambda() throws Exception {
+		super.testTryWithResources_lambda();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_2Resources() throws Exception {
+		super.testTryWithResources_2Resources();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_2NestedResources() throws Exception {
+		super.testTryWithResources_2NestedResources();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_2DiffResources() throws Exception {
+		super.testTryWithResources_2DiffResources();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_if1() throws Exception {
+		super.testTryWithResources_if1();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_if2() throws Exception {
+		super.testTryWithResources_if2();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_userClass() throws Exception {
+		super.testTryWithResources_userClass();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_isClosed1() throws Exception {
+		super.testTryWithResources_isClosed1();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_isClosed2() throws Exception {
+		super.testTryWithResources_isClosed2();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_userClassExceptionOnConstructor() throws Exception {
+		super.testTryWithResources_userClassExceptionOnConstructor();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_userClassExceptionOnClose() throws Exception {
+		super.testTryWithResources_userClassExceptionOnClose();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_finally() throws Exception {
+		super.testTryWithResources_finally();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_catch() throws Exception {
+		super.testTryWithResources_catch();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_catchFinally() throws Exception {
+		super.testTryWithResources_catchFinally();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_ExceptionInBody() throws Exception {
+		super.testTryWithResources_ExceptionInBody();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_2Resources_ExceptionInBody() throws Exception {
+		super.testTryWithResources_2Resources_ExceptionInBody();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_2Resources_ExceptionOnClose() throws Exception {
+		super.testTryWithResources_2Resources_ExceptionOnClose();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_2ResourcesCatch_01() throws Exception {
+		super.testTryWithResources_2ResourcesCatch_01();
+	}
+
+	@Test
+	@Override
+	public void testTryWithResources_2ResourcesCatch_02() throws Exception {
+		super.testTryWithResources_2ResourcesCatch_02();
 	}
 }
